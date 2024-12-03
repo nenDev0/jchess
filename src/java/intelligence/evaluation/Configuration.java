63e0234fec2e8d2c8ac1c7c3861f6ac083351b89
nenDev0 <sarni.rasilainen@gmail.com>
2024-12-03 16:45:37 +0100

    Javadoc: Configuration     - completed javadoc for Configuration class

--- src/java/intelligence/evaluation/Configuration.java
+++ src/java/intelligence/evaluation/Configuration.java
@@ -9,20 +9,39 @@ import src.java.engine.board.piecelib.Piece.PieceType;
 
 import java.util.Random;
 
+/**
+ *  Contains all Data used to add coefficients to the {@code EvalType}'s during evaluation.
+ * 
+ *  Global and piece-specific {@code EvalType}'s are saved separately within:
+ * 
+ *  {@link #map_overall_config},
+ *  {@link #map_piece_config}
+ * 
+ */
 public class Configuration
 {
 
-
-    private Setting state_setting;
-    private float[][] state_matrix;
+    /*
+     * used to reduce calculations, when requesting EvalType coefficients
+     */
+    private Setting state_setting; // used to calculate the state_matrix
+    private float[][] state_matrix; // used to create a 16x1 settings_vector for all settings
 
     private HashMap<EvalType, Setting> map_overall_config;
     private HashMap<PieceType, HashMap<EvalType, Setting>> map_piece_config;
 
-    private JsonHandler cfg_handler;
-    private boolean randomized;
+    private JsonHandler cfg_handler; // handles read and write of the config file
+    private boolean randomized; // enables this Configuration to have randomized values, based on the setting contained in the Config class
 
 
+    /**
+     *  Constructor
+     *  initializes new JsonHandler with {@code path} and reads the config of the given path
+     * 
+     * @param path
+     * @param randomized
+     * 
+     */
     public Configuration(String path, boolean randomized)
     {
         this.randomized = randomized;
@@ -32,41 +51,108 @@ public class Configuration
         m_read_config();
     }
 
+
+    /**
+     * 
+     * @return {@link #state_setting}
+     */
     public Setting state_setting()
     {
         return state_setting;
     }
 
+
+    /**
+     *  Returns {@code true}, if this Configuration is set to randomize values.
+     * 
+     *  <p> Useful for bot-training-mode
+     * 
+     * 
+     * @return {@code boolean}
+     */
     public boolean is_randomized()
     {
         return randomized;
     }
 
+
+    /**
+     *  Passes call over to {@link #cfg_handler}. This will overwrite the config file with the settings of this Configuration.
+     * 
+     * @param file_path
+     * @param type
+     * 
+     */
     public void write_config(String file_path, Type type)
     {
         cfg_handler.write_config(this, file_path, type);
     }
 
+
+    /**
+     *  Returns coefficient of the given {@code EvalType}, with regards to the current {@code piececount} and the handed {@code PieceType}
+     * 
+     * @param eval_type
+     * @param piece_implementation
+     * @param piececount
+     * 
+     * @return {@code float value}
+     */
     public float coefficient(EvalType eval_type, PieceType piece_implementation, int piececount)
     {
         return setting(eval_type, piece_implementation).get_value(piececount);
     }
 
+
+    /**
+     *  Returns coefficient of the given {@code EvalType}, with regards to the current {@code piececount}
+     * 
+     * @param eval_type
+     * @param piececount
+     * 
+     * @return {@code float value}
+     */
     public float coefficient(EvalType eval_type, int piececount)
     {
         return setting(eval_type).get_value(piececount);
     }
 
+
+    /**
+     *  Returns {@code Setting} of the given {@code EvalType}
+     *  
+     *  <p> Used for piece specific {@code EvalType}'s
+     * 
+     * @param eval_type
+     * @param piece_type
+     * 
+     * @return {@code Setting} // pulled from {@link #map_piece_config}
+     */
     public Setting setting(EvalType eval_type, PieceType piece_type)
     {
         return map_piece_config.get(piece_type).get(eval_type);
     }
 
+
+    /**
+     *  Returns {@code Setting} of the given {@code EvalType}
+     * 
+     *  <p> Used for global {@code EvalType}'s
+     * 
+     * @param eval_type
+     * 
+     * @return
+     */
     public Setting setting(EvalType eval_type)
     {
         return map_overall_config.get(eval_type);
     }
 
+
+    /**
+     *  executed during initialization.
+     * 
+     */
     private void m_read_config()
     {
 
@@ -94,8 +180,12 @@ public class Configuration
         m_normalize_values();
     }
 
+
     /**
-     * normalizes all values, so the bot configuration has a global maximum value equal to {@code 10}. 
+     *  normalizes all values, so the bot configuration has a global maximum value equal to {@code 10}. 
+     * 
+     *  <p> ensures the values in the config file stay within calculatable margins.
+     * 
      * 
      * @return void
      */
@@ -139,6 +229,14 @@ public class Configuration
         }
     }
 
+
+    /**
+     *  returns maximum value contained in this setting
+     * 
+     * @param setting
+     * 
+     * @return
+     */
     private float get_max(Setting setting)
     {
         float max = 0;
@@ -148,9 +246,15 @@ public class Configuration
         return max;
     }
 
+
+    /**
+     *  Creates the {@link #state_matrix}
+     *  used to evaluate how each value within a setting is weighed, according to the piececount.
+     * 
+     * 
+     */
     private void create_state_matrix()
     {
-
         state_matrix = new float[16][3];
         for (int piececount = 0; piececount < 16; piececount++)
         {
@@ -176,18 +280,20 @@ public class Configuration
                     state_matrix[piececount][i] = 0;
                 }
             }
-
         }
     }
 
+
     /**
+     *  fills in {@link #map_overall_config} and {@link #map_piece_config}
+     *  calls for {@link #cfg_handler} to read the config file and return specified values
      *  
      * @param eval_type
      * @param piece_implementation
+     * 
      */
     private void m_add_config(EvalType eval_type, PieceType piece_implementation)
     {
-
         float[] values;
         if (piece_implementation == null)
         {
@@ -230,10 +336,14 @@ public class Configuration
         }
     }
 
+
     /**
+     *  when {@link #randomized}{@code = true},
+     *  the handed values have a chance to have random deviations, based on values from the Config class.
      * 
-     * @param values
-     * @return
+     * @param values // values to randomize
+     * 
+     * @return handed values with potentially randomized deviations
      */
     private float[] m_randomize(float[] values)
     {
@@ -252,4 +362,6 @@ public class Configuration
         }
         return values;
     }
+
+
 }
